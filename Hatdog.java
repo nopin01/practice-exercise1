88db221308bcbfec69c1a39e3f897008de01aae5
nopin01 <delpinjohnanasco@gmail.com>
2025-02-26 13:36:25 +0800

    Add files via upload

---
+++ Hatdog.java
@@ -0,0 +1,32 @@
+package hatdog;
+
+import java.util.Scanner;
+
+
+public class Hatdog {
+
+      public static void main(String[] args) {
+          Scanner scanner = new Scanner(System.in);
+          
+          
+          int length = 0, width = 0;
+          
+          System.out.print("input length:");
+          length = scanner.nextInt();
+          
+          System.out.print("input width:");
+          width = scanner.nextInt();
+          
+          System.out.print("the area of rectangle is:" + width * length);
+          
+          
+          
+           
+          
+          
+         
+           
+             
+
+    }
+}
